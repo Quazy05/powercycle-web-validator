25ae2288861d19c1042df9ddeee8be119ed6fd76
AbyHakim <155170498+Quazy05@users.noreply.github.com>
2026-07-22 10:57:21 +0700

    update

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,6 +1,6 @@
 'use client';
 import { createContext, useContext, useState, useEffect } from 'react';
-import { auth } from '../../../lib/db';
+import { auth } from '@/lib/db';
 import { signOut, onAuthStateChanged } from 'firebase/auth';
 
 const AuthContext = createContext();
